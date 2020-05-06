57fa80a5a1209db56a160876a9d42630774fa6cf
adam <adamsiedlec@wp.pl>
2020-05-06 11:13:02 +0200

    adding files

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <title>Mem-serwis</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@700&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+    <div id="container">
+        <div id="header">
+            <h2>Mem-serwis by Adam Siedlecki</h2>
+            <h4>Najmniejszy zbiór memów w internecie</h4>
+            <div id="menu">
+                <ul>
+                    <li><a class="menu-item" href="index.php">STRONA GŁÓWNA</a></li>
+                    <li><a class="menu-item" href="index.php">PANEL ADMINA</a></li>
+                    <li><a class="menu-item" href="index.php">REJESTRACJA</a></li>
+                    <li><a class="menu-item" href="index.php">DODAJ MEMA</a></li>
+                    <li><a class="menu-item" href="index.php">WSZYSTKIE MEMY</a></li>
+                </ul>
+            </div>
+        </div>
+        <div id="content">
+
+        </div>
+    </div>
+
+</body>
+</html>
